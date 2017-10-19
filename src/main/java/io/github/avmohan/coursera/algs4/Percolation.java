4af348fb3a98a7eb1640379c05da21ffa679ce29
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2017-10-19 11:22:36 +0530

    Fix formatting (according to prescribed style)

--- src/main/java/io/github/avmohan/coursera/algs4/Percolation.java
+++ src/main/java/io/github/avmohan/coursera/algs4/Percolation.java
@@ -1,7 +1,6 @@
 package io.github.avmohan.coursera.algs4;
 
 import static edu.princeton.cs.algs4.StdOut.println;
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
@@ -9,191 +8,191 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  */
 public class Percolation {
 
-  // Encodes 3 values.
-  // 0th bit of every byte indicates whether the positionis open or closed
-  // 1st bit indicates if it's connected to top. (only relevant if it's a root)
-  // 2nd bit indicates if it's connected to bot. (only relevant if it's a root)
-  private byte flags[];
+    // Encodes 3 values.
+    // 0th bit of every byte indicates whether the positionis open or closed
+    // 1st bit indicates if it's connected to top. (only relevant if it's a root)
+    // 2nd bit indicates if it's connected to bot. (only relevant if it's a root)
+    private byte flags[];
 
-  // get k-th bit from right in flag[i]
-  private boolean getBit(int i, int k) {
-    return ((flags[i] >> k) & 1) == 1;
-  }
-
-  // set k-th bit from right in flag[i]
-  private void setBit(int i, int k) {
-    flags[i] |= 1 << k;
-  }
-
-  // Oth bit in flags used for grid
-  private boolean grid(int row, int col) {
-    return getBit(getNode(row, col), 0);
-  }
-
-  private void setGrid(int row, int col) {
-    setBit(getNode(row, col), 0);
-  }
-
-  // 1st bit in flags used for topConnected
-  private boolean topConnected(int node) {
-    return getBit(node, 1);
-  }
-
-  private void setTopConnected(int node) {
-    setBit(node, 1);
-  }
-
-  // 2nd bit in flags used for botConnected
-  private boolean botConnected(int node) {
-    return getBit(node, 2);
-  }
-
-  private void setBotConnected(int node) {
-    setBit(node, 2);
-  }
-
-  // size of grid
-  private final int n;
-
-  private int numberOfOpenSites;
-
-  // Will contain a node for each element in the grid.
-  private final WeightedQuickUnionUF mainUf;
-
-  // flag to indicate if system percolates.
-  private boolean percolates;
-
-  // Offsets for the 4 neighbours - top, right, bot, left
-  private static final int[] dRow = {-1, 0, 1, 0};
-  private static final int[] dCol = {0, 1, 0, -1};
-
-  // Create n-by-n grid, with all sites blocked
-  public Percolation(int n) {
-    if (n <= 0) {
-      throw new IllegalArgumentException("Grid size should be greater than zero.");
-    }
-    this.n = n;
-
-    this.mainUf = new WeightedQuickUnionUF(n * n);
-    this.flags = new byte[n * n];
-  }
-
-  // Open site (row, col) if it is not open already
-  public void open(int row, int col) {
-    validateBounds(row, col);
-    if (isOpen(row, col)) {
-      return;
+    // get k-th bit from right in flag[i]
+    private boolean getBit(int i, int k) {
+        return ((flags[i] >> k) & 1) == 1;
     }
 
-    // Open the site in grid.
-    setGrid(row, col);
-    numberOfOpenSites++;
-
-    // Find root node of current cell
-    // Root will always be the root node of the component containing the cell being opened.
-    int root = mainUf.find(getNode(row, col));
-
-    // If a top row cell is being opened, set topConnected to true.
-    if (row == 1) {
-      setTopConnected(root);
+    // set k-th bit from right in flag[i]
+    private void setBit(int i, int k) {
+        flags[i] |= 1 << k;
     }
 
-    // If a bot row cell is being opened, set botConnected to true.
-    if (row == n) {
-      setBotConnected(root);
+    // Oth bit in flags used for grid
+    private boolean grid(int row, int col) {
+        return getBit(getNode(row, col), 0);
     }
 
-    // If any of the neighbours are open, connect the corresponding nodes.
-    for (int i = 0; i < 4; i++) {
-      int neighbourRow = row + dRow[i];
-      int neighbourCol = col + dCol[i];
-      if (isValid(neighbourRow, neighbourCol) && isOpen(neighbourRow, neighbourCol)) {
-        // Find the root of the neighbour
-        int neighbourRoot = mainUf.find(getNode(neighbourRow, neighbourCol));
+    private void setGrid(int row, int col) {
+        setBit(getNode(row, col), 0);
+    }
 
-        // Merge the 2 components (Better use roots for merging as we have them anyway)
-        mainUf.union(root, neighbourRoot);
+    // 1st bit in flags used for topConnected
+    private boolean topConnected(int node) {
+        return getBit(node, 1);
+    }
 
-        // Update topConnected & botConnected - if either is connected, the new component will
-        // also be connected.
-        int newRoot = mainUf.find(getNode(row, col));
-        if (topConnected(root) || topConnected(neighbourRoot)) {
-          setTopConnected(newRoot);
+    private void setTopConnected(int node) {
+        setBit(node, 1);
+    }
+
+    // 2nd bit in flags used for botConnected
+    private boolean botConnected(int node) {
+        return getBit(node, 2);
+    }
+
+    private void setBotConnected(int node) {
+        setBit(node, 2);
+    }
+
+    // size of grid
+    private final int n;
+
+    private int numberOfOpenSites;
+
+    // Will contain a node for each element in the grid.
+    private final WeightedQuickUnionUF mainUf;
+
+    // flag to indicate if system percolates.
+    private boolean percolates;
+
+    // Offsets for the 4 neighbours - top, right, bot, left
+    private static final int[] dRow = {-1, 0, 1, 0};
+    private static final int[] dCol = {0, 1, 0, -1};
+
+    // Create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Grid size should be greater than zero.");
         }
-        if (botConnected(root) || botConnected(neighbourRoot)) {
-          setBotConnected(newRoot);
+        this.n = n;
+
+        this.mainUf = new WeightedQuickUnionUF(n * n);
+        this.flags = new byte[n * n];
+    }
+
+    // Open site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validateBounds(row, col);
+        if (isOpen(row, col)) {
+            return;
         }
-        root = newRoot;
-      }
+
+        // Open the site in grid.
+        setGrid(row, col);
+        numberOfOpenSites++;
+
+        // Find root node of current cell
+        // Root will always be the root node of the component containing the cell being opened.
+        int root = mainUf.find(getNode(row, col));
+
+        // If a top row cell is being opened, set topConnected to true.
+        if (row == 1) {
+            setTopConnected(root);
+        }
+
+        // If a bot row cell is being opened, set botConnected to true.
+        if (row == n) {
+            setBotConnected(root);
+        }
+
+        // If any of the neighbours are open, connect the corresponding nodes.
+        for (int i = 0; i < 4; i++) {
+            int neighbourRow = row + dRow[i];
+            int neighbourCol = col + dCol[i];
+            if (isValid(neighbourRow, neighbourCol) && isOpen(neighbourRow, neighbourCol)) {
+                // Find the root of the neighbour
+                int neighbourRoot = mainUf.find(getNode(neighbourRow, neighbourCol));
+
+                // Merge the 2 components (Better use roots for merging as we have them anyway)
+                mainUf.union(root, neighbourRoot);
+
+                // Update topConnected & botConnected - if either is connected, the new component will
+                // also be connected.
+                int newRoot = mainUf.find(getNode(row, col));
+                if (topConnected(root) || topConnected(neighbourRoot)) {
+                    setTopConnected(newRoot);
+                }
+                if (botConnected(root) || botConnected(neighbourRoot)) {
+                    setBotConnected(newRoot);
+                }
+                root = newRoot;
+            }
+        }
+        if (topConnected(root) && botConnected(root)) {
+            percolates = true;
+        }
     }
-    if (topConnected(root) && botConnected(root)) {
-      percolates = true;
+
+    // Is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validateBounds(row, col);
+        return grid(row, col);
     }
-  }
 
-  // Is site (row, col) open?
-  public boolean isOpen(int row, int col) {
-    validateBounds(row, col);
-    return grid(row, col);
-  }
-
-  // Is site (row, col) full?
-  public boolean isFull(int row, int col) {
-    validateBounds(row, col);
-    return topConnected(mainUf.find(getNode(row, col)));
-  }
-
-  // Number of open sites
-  public int numberOfOpenSites() {
-    return numberOfOpenSites;
-  }
-
-  // Does the system percolate?
-  public boolean percolates() {
-    return percolates;
-  }
-
-  private boolean isValid(int row, int col) {
-    return (row >= 1 && row <= n && col >= 1 && col <= n);
-  }
-
-  private void validateBounds(int row, int col) {
-    if (!isValid(row, col)) {
-      throw new IllegalArgumentException("Row or column not within bounds");
+    // Is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validateBounds(row, col);
+        return topConnected(mainUf.find(getNode(row, col)));
     }
-  }
 
-  // Get the union-find structure node for a given cell.
-  private int getNode(int row, int col) {
-    validateBounds(row, col);
-    return (row - 1) * n + col - 1;
-  }
+    // Number of open sites
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
 
-  public static void main(String[] args) {
-    Percolation p = new Percolation(2);
-    println(p.percolates());
-    p.open(1, 1);
-    p.open(2, 1);
-    println(p.percolates());
-    println(p.isFull(2, 1));
+    // Does the system percolate?
+    public boolean percolates() {
+        return percolates;
+    }
 
-    p = new Percolation(1);
-    println(p.percolates());
-    p.open(1, 1);
-    println(p.percolates());
+    private boolean isValid(int row, int col) {
+        return (row >= 1 && row <= n && col >= 1 && col <= n);
+    }
 
-    p = new Percolation(3);
-    p.open(1, 1);
-    p.open(3, 1);
-    println(p.percolates()); // false
-    println(p.isFull(1, 1)); // true
-    println(p.isFull(3, 1)); // false
-    p.open(1, 3);
-    p.open(2, 3);
-    println(p.percolates()); // false
-    p.open(3, 3);
-    println(p.percolates()); // true
-    println(p.isFull(3, 1)); // false
-  }
+    private void validateBounds(int row, int col) {
+        if (!isValid(row, col)) {
+            throw new IllegalArgumentException("Row or column not within bounds");
+        }
+    }
+
+    // Get the union-find structure node for a given cell.
+    private int getNode(int row, int col) {
+        validateBounds(row, col);
+        return (row - 1) * n + col - 1;
+    }
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(2);
+        println(p.percolates());
+        p.open(1, 1);
+        p.open(2, 1);
+        println(p.percolates());
+        println(p.isFull(2, 1));
+
+        p = new Percolation(1);
+        println(p.percolates());
+        p.open(1, 1);
+        println(p.percolates());
+
+        p = new Percolation(3);
+        p.open(1, 1);
+        p.open(3, 1);
+        println(p.percolates()); // false
+        println(p.isFull(1, 1)); // true
+        println(p.isFull(3, 1)); // false
+        p.open(1, 3);
+        p.open(2, 3);
+        println(p.percolates()); // false
+        p.open(3, 3);
+        println(p.percolates()); // true
+        println(p.isFull(3, 1)); // false
+    }
 
 }
--- src/main/java/io/github/avmohan/coursera/algs4/PercolationStats.java
+++ src/main/java/io/github/avmohan/coursera/algs4/PercolationStats.java
@@ -1,7 +1,6 @@
 package io.github.avmohan.coursera.algs4;
 
 import static edu.princeton.cs.algs4.StdOut.println;
-
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
@@ -10,82 +9,82 @@ import edu.princeton.cs.algs4.StdStats;
  */
 public class PercolationStats {
 
-  private final double mean;
-  private final double stddev;
-  private final double confidenceLo;
-  private final double confidenceHi;
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLo;
+    private final double confidenceHi;
 
-  // perform trials independent experiments on an n-by-n grid
-  public PercolationStats(int n, int trials) {
-    if (n <= 0 || trials <= 0) {
-      throw new IllegalArgumentException();
-    }
-    double[] results = new double[trials];
-    for (int trial = 0; trial < trials; trial++) {
-      Percolation percolation = new Percolation(n);
-      int[] openSequence = generateOpenSequence(n);
-      for (int cell : openSequence) {
-        percolation.open(getRow(cell, n), getCol(cell, n));
-        if (percolation.percolates()) {
-          break;
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
         }
-      }
-      results[trial] = (percolation.numberOfOpenSites() * 1.0) / (n * n);
+        double[] results = new double[trials];
+        for (int trial = 0; trial < trials; trial++) {
+            Percolation percolation = new Percolation(n);
+            int[] openSequence = generateOpenSequence(n);
+            for (int cell : openSequence) {
+                percolation.open(getRow(cell, n), getCol(cell, n));
+                if (percolation.percolates()) {
+                    break;
+                }
+            }
+            results[trial] = (percolation.numberOfOpenSites() * 1.0) / (n * n);
+        }
+        mean = StdStats.mean(results);
+        stddev = StdStats.stddev(results);
+        double confidenceDelta = (1.96 * stddev) / Math.sqrt(trials);
+        confidenceLo = mean - confidenceDelta;
+        confidenceHi = mean + confidenceDelta;
     }
-    mean = StdStats.mean(results);
-    stddev = StdStats.stddev(results);
-    double confidenceDelta = (1.96 * stddev) / Math.sqrt(trials);
-    confidenceLo = mean - confidenceDelta;
-    confidenceHi = mean + confidenceDelta;
-  }
 
-  private int[] generateOpenSequence(int n) {
-    int[] sequence = new int[n * n];
-    for (int i = 0; i < n * n; i++) {
-      sequence[i] = i + 1;
+    private int[] generateOpenSequence(int n) {
+        int[] sequence = new int[n * n];
+        for (int i = 0; i < n * n; i++) {
+            sequence[i] = i + 1;
+        }
+        StdRandom.shuffle(sequence);
+        return sequence;
     }
-    StdRandom.shuffle(sequence);
-    return sequence;
-  }
 
-  private static int getRow(int cell, int n) {
-    return (cell - 1) / n + 1;
-  }
+    private static int getRow(int cell, int n) {
+        return (cell - 1) / n + 1;
+    }
 
-  private static int getCol(int cell, int n) {
-    return cell % n + 1;
-  }
+    private static int getCol(int cell, int n) {
+        return cell % n + 1;
+    }
 
-  // sample mean of percolation threshold
-  public double mean() {
-    return mean;
-  }
+    // sample mean of percolation threshold
+    public double mean() {
+        return mean;
+    }
 
-  // sample standard deviation of percolation threshold
-  public double stddev() {
-    return stddev;
-  }
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return stddev;
+    }
 
-  // low  endpoint of 95% confidence interval
-  public double confidenceLo() {
-    return confidenceLo;
-  }
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return confidenceLo;
+    }
 
-  // high endpoint of 95% confidence interval
-  public double confidenceHi() {
-    return confidenceHi;
-  }
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return confidenceHi;
+    }
 
-  // test client (described below)
-  public static void main(String[] args) {
-    int n = Integer.parseInt(args[0]);
-    int trials = Integer.parseInt(args[1]);
-    PercolationStats stats = new PercolationStats(n, trials);
-    println(String.format("%-23s", "mean") + " = " + stats.mean());
-    println(String.format("%-23s", "stddev") + " = " + stats.stddev());
-    println(String.format("%-23s",
-        "95% confidence interval") + " = [" + stats.confidenceLo() + ", " + stats.confidenceHi()
-        + "]");
-  }
+    // test client (described below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, trials);
+        println(String.format("%-23s", "mean") + " = " + stats.mean());
+        println(String.format("%-23s", "stddev") + " = " + stats.stddev());
+        println(String.format("%-23s",
+            "95% confidence interval") + " = [" + stats.confidenceLo() + ", " + stats.confidenceHi()
+            + "]");
+    }
 
 }
