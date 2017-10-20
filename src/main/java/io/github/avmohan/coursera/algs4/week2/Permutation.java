8d144f500489fed98a2cbec0786fc038ace902b4
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2017-10-20 10:42:11 +0530

    Use reservoir sampling to reduce memory usage

--- src/main/java/io/github/avmohan/coursera/algs4/week2/Permutation.java
+++ src/main/java/io/github/avmohan/coursera/algs4/week2/Permutation.java
@@ -2,6 +2,7 @@ package io.github.avmohan.coursera.algs4.week2;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 /**
  * Created by avmohan on 20/10/17.
@@ -10,13 +11,24 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> strings = new RandomizedQueue<>();
+        // Reservoir sampling.
+        // Add first k items. Starting from (k+1)th element, for the ith element, replace a random
+        // element in the queue with the new ith element with probability k/i
+        int i = 0;
         while (!StdIn.isEmpty()) {
-            strings.enqueue(StdIn.readString());
+            i++;
+            String string = StdIn.readString();
+            if (i <= k) {
+                strings.enqueue(string);
+            } else {
+                if (StdRandom.uniform() < ((double) k) / i) {
+                    strings.dequeue();
+                    strings.enqueue(string);
+                }
+            }
         }
         for (String string : strings) {
-            if (k == 0) break;
             StdOut.println(string);
-            k--;
         }
     }
 }
