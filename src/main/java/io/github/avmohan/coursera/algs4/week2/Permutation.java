37b5b9fc67a9f54d39f639358a541c96767f164b
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2017-10-20 10:03:11 +0530

    Read input till EOF without using NSE exception to break

--- src/main/java/io/github/avmohan/coursera/algs4/week2/Permutation.java
+++ src/main/java/io/github/avmohan/coursera/algs4/week2/Permutation.java
@@ -2,7 +2,6 @@ package io.github.avmohan.coursera.algs4.week2;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import java.util.NoSuchElementException;
 
 /**
  * Created by avmohan on 20/10/17.
@@ -11,12 +10,8 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> strings = new RandomizedQueue<>();
-        while (true) {
-            try {
-                strings.enqueue(StdIn.readString());
-            } catch (NoSuchElementException e) {
-                break;
-            }
+        while (!StdIn.isEmpty()) {
+            strings.enqueue(StdIn.readString());
         }
         for (String string : strings) {
             if (k == 0) break;
