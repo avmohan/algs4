494938cb8ba9273a41473b44b1d51647f79d6551
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2018-01-16 09:23:57 +0530

    Add bounding rect to each node

--- src/main/java/io/github/avmohan/coursera/algs4/part1/week5/KdTree.java
+++ src/main/java/io/github/avmohan/coursera/algs4/part1/week5/KdTree.java
@@ -16,15 +16,35 @@ public class KdTree {
         return (depth % 2 == 0) ? X_COMP : Y_COMP;
     }
 
-    // Each node corresponds to an axis-aligned rectangle in the unit square, which encloses all of the points in its subtree
+    // Find the cut of curRect that is made by p
+    private static RectHV rect(RectHV curRect, Point2D p, int depth, int direction) {
+        RectHV newRect;
+        if (depth % 2 == 0) {
+            if (direction == 0)
+                // left
+                newRect = new RectHV(curRect.xmin(), curRect.ymin(), p.x(), curRect.ymax());
+            else
+                // right
+                newRect = new RectHV(p.x(), curRect.ymin(), curRect.xmax(), curRect.ymax());
+        } else {
+            if (direction == 0)
+                // bottom
+                newRect = new RectHV(curRect.xmin(), curRect.ymin(), curRect.xmax(), p.y());
+            else
+                // top
+                newRect = new RectHV(curRect.xmin(), p.y(), curRect.xmax(), curRect.ymax());
+        }
+        return newRect;
+    }
 
+    // Each node corresponds to an axis-aligned rectangle in the unit square, which encloses all of the points in its subtree
     private static class Node {
         private Point2D p;      // the point
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
         private Node lb;        // the left/bottom subtree
         private Node rt;        // the right/top subtree
 
-        public Node(Point2D p) {
+        public Node(Point2D p, RectHV rect) {
             this.p = p;
         }
     }
@@ -49,26 +69,32 @@ public class KdTree {
     // Add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException("Point cannot be null in insert()");
-        root = insert(root, p, 0);
+        RectHV rect = new RectHV(0, 0, 1, 1);
+        root = insert(root, p, rect, 0);
     }
 
     // Insert point p into the (possibly null) subtree rooted at node, at given depth, and return the new subtree
-    private Node insert(Node node, Point2D p, int depth) {
+    private Node insert(Node node, Point2D p, RectHV rect, int depth) {
         // Reached leaf without finding p, so create a new node and add p
         if (node == null) {
             size++;
-            return new Node(p);
+            return new Node(p, rect);
         }
 
         // Tree contains p so don't add again.
-        if (p.equals(node.p))
+        if (p.equals(node.p)) {
+            assert rect.equals(node.rect);
             return node;
+        }
 
         // Add on left or right subtree based on comparison of appropriate dimension
-        if (comparator(depth).compare(p, node.p) <= 0)
-            node.lb = insert(node.lb, p, depth + 1);
-        else
-            node.rt = insert(node.rt, p, depth + 1);
+        if (comparator(depth).compare(p, node.p) <= 0) {
+            RectHV newRect = rect(rect, p, depth, 0);
+            node.lb = insert(node.lb, p, newRect, depth + 1);
+        } else {
+            RectHV newRect = rect(rect, p, depth, 1);
+            node.rt = insert(node.rt, p, newRect, depth + 1);
+        }
         return node;
     }
 
